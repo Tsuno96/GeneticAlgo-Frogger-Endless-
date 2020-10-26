961aa35f75387522a4211495ac3aaf79e73ab7d9
LorenzoMarnat <lmarnat14@gmail.com>
2020-10-26 15:21:42 +0100

    Player chooses his move using neural network

--- Endless_Frogger-AlgoGenetic/Assets/Player.cs
+++ Endless_Frogger-AlgoGenetic/Assets/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics;
 
 public class Player : MonoBehaviour
 {
@@ -14,12 +15,14 @@ public class Player : MonoBehaviour
 
     public Matrix<float> inputLayers;
 
+    public bool isDead;
     List<Lane> lstLanes;
 
     NeuralNetwork neuralNetwork;
     // Start is called before the first frame update
     void Start()
     {
+        isDead = false;
         lstLanes = GameObject.FindGameObjectWithTag("LaneGenerator").GetComponent<LaneGenerator>().lstLanes;
         //GetNeighbors();
         StartCoroutine("MakeDecision");
@@ -37,17 +40,17 @@ public class Player : MonoBehaviour
     {
         yield return new WaitForSeconds(2f);
 
-        while (currentLane != 10 && steps != 20)
+        while (currentLane != 10 && steps != 20 && !isDead)
         {
             GetNeighbors();
 
             Matrix<float> outputs = neuralNetwork.RunNetwork(inputs);
-            Debug.Log(outputs);
+            //Debug.Log(outputs);
 
-            if (inputs[3] == 0)
-            {
-                MoveForward();
-            }
+            int move = MaxOutputs(outputs);
+            ChooseMove(move);
+
+            CheckDeath();
 
             steps++;
 
@@ -130,9 +133,62 @@ public class Player : MonoBehaviour
         }
     }
 
+    void ChooseMove(int index)
+    {
+        switch(index)
+        {
+            case 0: MoveLeft();
+                break;
+            case 1: MoveRight();
+                break;
+            case 3: MoveForward();
+                break;
+            default: break;
+        }
+    }
+    int MaxOutputs(Matrix<float> outputs)
+    {
+        float max = -2;
+        int index = -1;
+        for (int i = 0; i < outputs.ColumnCount; i++)
+            if (outputs[0, i] > max)
+            {
+                index = i;
+                max = outputs[0, i];
+            }
+        return index;
+    }
     void MoveForward()
     {
         transform.position += new Vector3(1,0,0);
         currentLane++;
+    }
+    void MoveLeft()
+    {
+        transform.position += new Vector3(0, 0, 1);
+        currentCube--;
+    }
+    void MoveRight()
+    {
+
+            transform.position += new Vector3(0, 0, -1);
+            currentCube++;
+    }
+
+    void CheckDeath()
+    {
+        if (currentCube < 0 || currentCube > 9)
+        {
+            Debug.Log("Dead");
+            isDead = true;
+            GetComponent<Renderer>().enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Dead");
+        isDead = true;
+        GetComponent<Renderer>().enabled = false;
     }
 }
